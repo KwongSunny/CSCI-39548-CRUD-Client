b77a7e7822ad19c3b8aade1c0642036f74c7d6e6
quachjustin <jquachh7@gmail.com>
2022-05-17 19:21:44 -0400

    added homepage background

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,12 +4,26 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import { withWidth } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import campusImage from "../../photos/campus.png"
+const useStyles = makeStyles(theme => ({
+  
+}));
+
 const HomePageView = () => {
+  const classes = useStyles();
+
   // Render Home page view
   return (
-    <div >
-      <h1>Home Page</h1>
+    <div className = {classes.homePage}  >
+      <h1>Campus Home Page</h1>
+      <div style={{width:"100%"}}>
+      <img src= {campusImage}width="100%"/>
+      </div>
     </div>
+    
+    
   );    
 }
 
